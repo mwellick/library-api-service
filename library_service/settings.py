ada9233b70fa348a2fe3b0888c13de7e4ccd2e71
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-08-01 12:11:22 +0300

    Added 'library service','user' apps and DRF in project settings installed apps

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "library_service",
+    "user"
 ]
 
 MIDDLEWARE = [
