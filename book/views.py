fd95182e15db4e1dcec278c795cb68485d6916a0
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-08-02 18:10:03 +0300

    Added router,views with permissions for book app

--- book/views.py
+++ book/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from .models import Book
+from .serializers import BookSerializer, BookListSerializer, BookRetrieveSerializer
 
-# Create your views here.
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            permission_classes = [IsAdminUser]
+        elif self.action == "list":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def get_serializer_class(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return BookRetrieveSerializer
+        elif self.action == "list":
+            return BookListSerializer
+        return BookSerializer
