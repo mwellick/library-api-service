8aeba493b2e52d5df1802ccd4580e976767a45bd
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-08-07 20:40:33 +0300

    Added an ability to send notifications throught Telegram after borrowing create and return

--- borrowing/views.py
+++ borrowing/views.py
@@ -12,6 +12,7 @@ from .serializers import (
     ReturnBookSerializer
 )
 from .models import Borrowing
+from tg_notifications.notifications import send_message
 
 
 class BorrowingViewSet(ModelViewSet):
@@ -20,7 +21,16 @@ class BorrowingViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        borrowing = serializer.save(user=self.request.user)
+        message = (f"New borrowing has been created\n"
+                   f"Borrow id: {borrowing.id}\n"
+                   f"Author: {borrowing.book.author}\n"
+                   f"Book: {borrowing.book.title} (cover: {borrowing.book.cover})\n"
+                   f"User: {borrowing.user.email}\n"
+                   f"Return date {borrowing.expected_return_date}\n"
+                   f"Daily fee {borrowing.book.daily_fee}$")
+
+        send_message(message)
 
     def get_queryset(self):
         queryset = self.queryset
@@ -57,6 +67,11 @@ class BorrowingViewSet(ModelViewSet):
     @transaction.atomic()
     def return_book(self, request, pk=None):
         borrowing = self.get_object()
+        book_id = request.data.get("borrowing__book_id")
+        if not book_id or int(book_id) != borrowing.book.id:
+            return Response(
+                {"detail": "Choose a correct book"}
+            )
         if borrowing.actual_return_date is not None:
             return Response(
                 {"detail": "You have already returned this book"},
@@ -74,4 +89,10 @@ class BorrowingViewSet(ModelViewSet):
         borrowing.actual_return_date = timezone.now()
         borrowing.save()
         book.save()
+        message = (f"{borrowing.user.email} has returned book "
+                   f"{borrowing.book.title}(cover: {borrowing.book.cover}) "
+                   f"from {borrowing.borrow_date}\n"
+                   f"Borrowing id: {borrowing.id}\n"
+                   f"Return date: {borrowing.actual_return_date}")
+        send_message(message)
         return Response(serializer.data, status=status.HTTP_200_OK)
