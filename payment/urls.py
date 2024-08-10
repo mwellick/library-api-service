a12e962ee2c4f36efa617121380911c4e2809cd4
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-08-10 13:09:56 +0300

    Created success and cancel views and endpoints

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PaymentViewSet
+from .views import (
+    PaymentViewSet,
+    PaymentSuccessView,
+    PaymentCancelView
+)
 
 router = routers.DefaultRouter()
 router.register("", PaymentViewSet)
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "success?session_id={CHECKOUT_SESSION_ID}",
+        PaymentSuccessView.as_view(),
+        name="payment-success"
+    ),
+    path(
+        "cancel/",
+        PaymentCancelView.as_view(),
+        name="payment-cancel"
+    ),
+]
 
 app_name = "payments"
--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,9 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from .models import Payment
 from .serializers import (
     PaymentSerializer,
@@ -27,3 +31,23 @@ class PaymentViewSet(ModelViewSet):
         elif self.action == "retrieve":
             return PaymentRetrieveSerializer
         return PaymentSerializer
+
+
+class PaymentSuccessView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "status": "Payment successful."
+            },
+            status=status.HTTP_200_OK
+        )
+
+
+class PaymentCancelView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "status": "Payment canceled.Please, complete your payment within 24 hours."
+            },
+            status=status.HTTP_200_OK
+        )
