080c5b5b40710314a07bb3b19d14540270e2b8a5
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-08-12 19:52:13 +0300

    Created checker task of expired payment status

---
+++ payment/tasks.py
@@ -0,0 +1,24 @@
+import os
+import stripe
+from celery import shared_task
+from dotenv import load_dotenv
+from .models import Payment
+
+load_dotenv()
+
+
+@shared_task()
+def check_session_for_expiration():
+    stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
+
+    pending_payments = Payment.objects.filter(
+        status="PENDING"
+    )
+
+    for payment in pending_payments:
+        stripe_session = stripe.checkout.Session.retrieve(
+            payment.session_id
+        )
+        if stripe_session.payment_status == "expired":
+            payment.status = payment.Status.EXPIRED
+            payment.save()
